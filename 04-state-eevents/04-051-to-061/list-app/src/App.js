d7a69a442d2e98f29893e7a34a789e95b2acfd83
TohidEq <TohidEq@gmail.com>
2022-09-28 02:58:52 +0330

    CSS Modules NetNinja 06-006

--- 04-state-eevents/04-051-to-061/list-app/src/App.js
+++ 04-state-eevents/04-051-to-061/list-app/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShowCards from "./components/ShowCards/ShowCards";
 import Selecting from "./components/Select/Selecting";
+import StyleModule from "./components/StyleModule/StyleModule";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ShowCards />} />
           <Route path="/select" element={<Selecting />} />
+          <Route path="/stylemodule" element={<StyleModule />} />
         </Routes>
       </div>
     </Router>
---
+++ 04-state-eevents/04-051-to-061/list-app/src/components/StyleModule/StyleModule.js
@@ -0,0 +1,10 @@
+import style from "./StyleModule.module.css";
+const StyleModule = () => {
+  return (
+    <div className="">
+      <button className={style.btn}>test btn</button>
+    </div>
+  );
+};
+
+export default StyleModule;
