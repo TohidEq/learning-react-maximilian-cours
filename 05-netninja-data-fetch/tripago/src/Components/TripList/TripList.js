5550fa9f9ea41235a9bd59e892e41d4a3def03c3
TohidEq <TohidEq@gmail.com>
2022-10-03 23:33:49 +0330

    08-003-fetching-data-with-useEffect  NetNinja

--- 05-netninja-data-fetch/tripago/src/Components/TripList/TripList.js
+++ 05-netninja-data-fetch/tripago/src/Components/TripList/TripList.js
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const TripList = () => {
-  /*
   const [trips, setTrips] = useState();
 
-  //console.log(trips, "dsa");
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, []);
 
-  fetch("http://localhost:3000/trips")
-    .then((response) => response.json())
-    .then((json) => setTrips(json));
-
-    */
+  console.log(trips, "dsa");
 
   return (
     <div>
       <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li>{trip.id}</li>
+        ))}
+      </ul>
     </div>
   );
 };
