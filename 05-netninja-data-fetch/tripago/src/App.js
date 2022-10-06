3b3ad797b5d308f16bf9e38b023609f3dee01a51
TohidEq <TohidEq@gmail.com>
2022-10-06 10:23:49 +0330

    08-004- fix problems NetNinja

--- 05-netninja-data-fetch/tripago/src/App.js
+++ 05-netninja-data-fetch/tripago/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import TripList from "./Components/TripList/TripList";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<TripList />} />
 
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
--- 05-netninja-data-fetch/tripago/src/Components/TripList/TripList.js
+++ 05-netninja-data-fetch/tripago/src/Components/TripList/TripList.js
@@ -1,21 +1,24 @@
 import { useState, useEffect } from "react";
 const TripList = () => {
   const [trips, setTrips] = useState();
-  const [url, setUrl] = useState("http://localhost:3000/trips");
+  const [url, setUrl] = useState("http://localhost:3001/trips");
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((json) => setTrips(json));
+      .then((json) => {
+        setTrips(json);
+      });
   }, [url]);
 
   console.log(trips, "dsa");
   const clickHandler = () => {
-    setUrl("http://localhost:3000/trips?id=1");
+    setUrl("http://localhost:3001/trips?id=1");
   };
 
   const clickHandler2 = () => {
-    setUrl("http://localhost:3000/trips");
+    setUrl("http://localhost:3001/trips");
+    setUrl("http://localhost:3001/trips");
   };
 
   return (
@@ -23,7 +26,9 @@ const TripList = () => {
       <h2>Trip List</h2>
       <ul>
         {trips.map((trip) => (
-          <li>{trip.id}</li>
+          <li>
+            id:{trip.id}, title:{trip.id}
+          </li>
         ))}
       </ul>
 
